ccafdbd458263e643e23c6dcab9652d161e5154c
nii_kenichi <cm10030@gmail.com>
2014-12-30 02:11:33 +0900

    First commit

---
+++ lib/hello-nokogiri.rb
@@ -0,0 +1,25 @@
+require 'open-uri'
+require 'nokogiri'
+require 'fileutils'
+
+root_url = 'http://blog.livedoor.jp/chihhylove'
+
+doc = Nokogiri::HTML(open("#{root_url}/archives/8660418.html"))
+
+local_dir = "#{File.dirname(__FILE__)}/../tmp"
+
+FileUtils.mkdir_p(local_dir) unless File.exist?(local_dir)
+
+doc.xpath('//*[@id="articlebody"]//img[@class="image pict"]/@src').each do |node|
+  img_content = open(node).read
+  file_name = File.join(local_dir, File.basename(node))
+  if File.exist?(file_name)
+    puts "file was saved. #{file_name}"
+    next
+  end
+  File.open(file_name, 'w') do |file|
+    file.write(img_content)
+  end
+  puts "saved... #{file_name}"
+  sleep 1
+end
